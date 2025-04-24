a201b9282f34d50b9843497ca69a05e875ccf15e
Sofia Håbrekke <sofiash@stud.ntnu.no>
2025-04-24 08:57:25 +0200

    Initial commit

---
+++ src/main/java/edu/ntnu/idatt2106/krisefikser/KrisefikserApplication.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idatt2106.krisefikser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrisefikserApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KrisefikserApplication.class, args);
+  }
+
+}
