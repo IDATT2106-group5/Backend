4c18eda11e25406461cc305e0ea389dc2ba041ce
Sofia Håbrekke <sofiash@stud.ntnu.no>
2025-04-25 16:04:13 +0200

    feat: edit name of method for editing members

--- src/main/java/edu/ntnu/idatt2106/krisefikser/persistance/repository/HouseholdRepository.java
+++ src/main/java/edu/ntnu/idatt2106/krisefikser/persistance/repository/HouseholdRepository.java
@@ -5,15 +5,24 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * The interface Household repository.
+ */
 @Repository
 public interface HouseholdRepository extends JpaRepository<Household, Long> {
-    /**
-     * Find a household by its unique name.
-     *
-     * @param name the name to search for
-     * @return an Optional containing the Household if found
-     */
-    Optional<Household> findByName(String name);
+  /**
+   * Find a household by its unique name.
+   *
+   * @param name the name to search for
+   * @return an Optional containing the Household if found
+   */
+  Optional<Household> findByName(String name);
 
-    void editNumberOfMembers(Long id, int numberOfMembers);
+  /**
+   * Update number of members by id.
+   *
+   * @param id              the id
+   * @param numberOfMembers the number of members
+   */
+  void setNumberOfMembers(Long id, int numberOfMembers);
 }
