f6378b623d7c6bdec30e765c9f49722e6320c2ff
Sofia Håbrekke <sofiash@stud.ntnu.no>
2025-04-25 15:48:51 +0200

    fix: remove lombok from entity classes

--- src/main/java/edu/ntnu/idatt2106/krisefikser/persistance/entity/Household.java
+++ src/main/java/edu/ntnu/idatt2106/krisefikser/persistance/entity/Household.java
@@ -8,15 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "household")
 public class Household {
@@ -37,4 +29,54 @@ public class Household {
   @OneToOne
   @JoinColumn(nullable = false)
   private User owner;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public int getNumberOfMembers() {
+    return numberOfMembers;
+  }
+
+  public void setNumberOfMembers(int numberOfMembers) {
+    this.numberOfMembers = numberOfMembers;
+  }
+
+  public User getOwner() {
+    return owner;
+  }
+
+  public void setOwner(User owner) {
+    this.owner = owner;
+  }
+
+  public Household() {
+  }
+
+  public Household(String name, String address, int numberOfMembers, User owner) {
+    this.name = name;
+    this.address = address;
+    this.numberOfMembers = numberOfMembers;
+    this.owner = owner;
+  }
 }
