15977e6025f778228a77fe0177653363ca139db3
Edvard Granheim Harbo <edvargh@stud.ntnu.no>
2025-04-26 09:06:33 +0200

    feat: linked up backend frontend register

--- src/main/java/edu/ntnu/idatt2106/krisefikser/api/controller/AuthController.java
+++ src/main/java/edu/ntnu/idatt2106/krisefikser/api/controller/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
   private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
