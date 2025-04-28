2f82e340796762d84926020ed036e09fe5601a7c
Sofia Håbrekke <sofiash@stud.ntnu.no>
2025-04-28 11:47:53 +0200

    fix: change requestbody to requestparam for get

--- src/main/java/edu/ntnu/idatt2106/krisefikser/api/controller/HouseholdController.java
+++ src/main/java/edu/ntnu/idatt2106/krisefikser/api/controller/HouseholdController.java
@@ -18,12 +18,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Household", description = "Endpoints for managing a household")
 @RestController
 @RequestMapping("/api/household")
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+//@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class HouseholdController {
   HouseholdService householdService;
   private static final Logger LOGGER = LoggerFactory.getLogger(HouseholdController.class);
@@ -124,9 +125,9 @@ public class HouseholdController {
   }
 
   @Operation(summary = "Gets the members of a household", description = "Gets the members of a household with the given ID")
-  @GetMapping("/get-members")
+  @GetMapping("/members")
   public ResponseEntity<Map<String, Object>> getHouseholdMembers(
-      @RequestBody Long householdId) {
+      @RequestParam Long householdId) {
     try {
       Map<String, Object> members = householdService.getMembersByHouseholdId(householdId);
       LOGGER.info("Household members retrieved successfully: {}", members);
