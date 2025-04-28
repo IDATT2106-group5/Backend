82d932afd91ce18ae8b4f517683cf0b361f56467
Tam Le <tamle2107@hotmail.com>
2025-04-28 14:14:37 +0200

    refactor: fixed the way user information gets sendt to frontend

--- src/main/java/edu/ntnu/idatt2106/krisefikser/service/UserService.java
+++ src/main/java/edu/ntnu/idatt2106/krisefikser/service/UserService.java
@@ -16,8 +16,9 @@ public class UserService {
     this.userRepository = userRepository;
   }
 
-  public User getUserByConfirmationToken(String confirmationToken) {
-    return userRepository.findByConfirmationToken(confirmationToken)
+
+  public User getUserByEmail(String email) {
+    return userRepository.findByEmail(email)
         .orElseThrow(() -> new RuntimeException("User not found"));
   }
 }
