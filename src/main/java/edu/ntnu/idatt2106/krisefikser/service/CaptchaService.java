941586d1a9debe58f8c682419e2e1b7b74b6394c
Markush <markunha@stud.ntnu.no>
2025-04-28 14:23:15 +0200

    feat/test: Added unit testing class for CaptchaService class. Had to rewrite the service class to change the logic to allow for injection of resttemplate

--- src/main/java/edu/ntnu/idatt2106/krisefikser/service/CaptchaService.java
+++ src/main/java/edu/ntnu/idatt2106/krisefikser/service/CaptchaService.java
@@ -1,33 +1,44 @@
 package edu.ntnu.idatt2106.krisefikser.service;
 
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-
-import java.util.Map;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CaptchaService {
 
-    @Value("${hcaptcha.secret}")
-    private String hCaptchaSecret;
+  private static final String VERIFY_URL = "https://hcaptcha.com/siteverify";
+  private final RestTemplate restTemplate;
+  @Value("${hcaptcha.secret}")
+  private String hCaptchaSecret;
 
-    private static final String VERIFY_URL = "https://hcaptcha.com/siteverify";
+  @Autowired
+  public CaptchaService(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
 
-    public boolean verifyToken(String token) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("secret", hCaptchaSecret);
-        params.add("response", token);
-
-        ResponseEntity<Map> response = restTemplate.postForEntity(VERIFY_URL, params, Map.class);
-        Map<String, Object> body = response.getBody();
-
-        return body != null && Boolean.TRUE.equals(body.get("success"));
+  public boolean verifyToken(String token) {
+    if (token == null || token.isEmpty()) {
+      return false;
     }
-}
 
+    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    params.add("secret", hCaptchaSecret);
+    params.add("response", token);
+
+    try {
+      ResponseEntity<Map> response = restTemplate.postForEntity(VERIFY_URL, params, Map.class);
+      Map<String, Object> body = response.getBody();
+
+      return body != null && Boolean.TRUE.equals(body.get("success"));
+    } catch (RestClientException e) {
+      return false;
+    }
+  }
+}
