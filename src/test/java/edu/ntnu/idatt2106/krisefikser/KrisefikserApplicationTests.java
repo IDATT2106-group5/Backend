c20335acacebeafe845c8e151dbc6c1fde8c6637
Jon Bergland <jobergl@stud.ntnu.no>
2025-05-02 09:55:33 +0200

    build: add a TestConfig file for H2-testdatabase setup

--- src/test/java/edu/ntnu/idatt2106/krisefikser/KrisefikserApplicationTests.java
+++ src/test/java/edu/ntnu/idatt2106/krisefikser/KrisefikserApplicationTests.java
@@ -1,13 +1,19 @@
 package edu.ntnu.idatt2106.krisefikser;
 
+import edu.ntnu.idatt2106.krisefikser.config.TestConfig;
+import edu.ntnu.idatt2106.krisefikser.config.TestJwtConfig;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = {TestConfig.class, TestJwtConfig.class})
+@ActiveProfiles("test")
 class KrisefikserApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
-
+    @Test
+    void contextLoads() {
+    }
 }
