b4709598de6b7989c3af11ed1982ec182ac9915c
Edvard Granheim Harbo <edvargh@stud.ntnu.no>
2025-05-02 08:41:09 +0200

    feat: finished most of incident logic

--- src/main/java/edu/ntnu/idatt2106/krisefikser/api/controller/IncidentController.java
+++ src/main/java/edu/ntnu/idatt2106/krisefikser/api/controller/IncidentController.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/incidents")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class IncidentController {
 
   private static final Logger logger = LoggerFactory.getLogger(IncidentController.class);
