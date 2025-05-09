617a5afdc26a77a7478a3ff314bab753bfe63257
Jon Bergland <jobergl@stud.ntnu.no>
2025-05-09 10:57:31 +0200

    test: add TestEmailConfig for JavaMailSender configuration in test profile

---
+++ src/test/java/edu/ntnu/idatt2106/krisefikser/config/TestEmailConfig.java
@@ -0,0 +1,22 @@
+package edu.ntnu.idatt2106.krisefikser.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+@Profile("test")
+public class TestEmailConfig {
+
+    @Bean
+    @Primary
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("localhost");
+        mailSender.setPort(3025);
+        return mailSender;
+    }
+}
